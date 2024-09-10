5b6498c0531d68560947655c66a9a6d8f516c809
Zach Mosher <zmosher4@gmail.com>
2024-09-10 13:46:26 -0500

    all chapters completed

--- src/components/ApplicationViews.jsx
+++ src/components/ApplicationViews.jsx
@@ -1,44 +1,73 @@
-import { useState } from 'react'
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { Authorized } from "./Authorized"
-import { Login } from "../pages/Login.jsx"
-import Home from "../pages/Home"
-import { RockForm } from "./RockForm.jsx"
-import { RockList } from "./RockList.jsx"
-import { Register } from '../pages/Register.jsx'
-
+import { useEffect, useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Authorized } from './Authorized';
+import { Login } from '../pages/Login.jsx';
+import Home from '../pages/Home';
+import { RockForm } from './RockForm.jsx';
+import { RockList } from './RockList.jsx';
+import { Register } from '../pages/Register.jsx';
 
 export const ApplicationViews = () => {
-    const [rocksState, setRocksState] = useState([{
+  const [rocksState, setRocksState] = useState([
+    {
+      id: 1,
+      name: 'Sample',
+      type: {
         id: 1,
-        name: "Sample",
-        type: {
-            id: 1,
-            label: "Volcanic"
-        }
-    }])
+        label: 'Volcanic',
+      },
+    },
+  ]);
 
-    const fetchRocksFromAPI = async () => {
-        const response = await fetch("http://localhost:8000/rocks",
-            {
-                headers: {
-                    Authorization: `Token ${JSON.parse(localStorage.getItem("rock_token")).token}`
-                }
-            })
-        const rocks = await response.json()
-        setRocksState(rocks)
+  const fetchRocksFromAPI = async (showAll) => {
+    let url = 'http://localhost:8000/rocks';
+    if (!showAll) {
+      url = 'http://localhost:8000/rocks?owner=current';
     }
+    const response = await fetch(url, {
+      headers: {
+        Authorization: `Token ${
+          JSON.parse(localStorage.getItem('rock_token')).token
+        }`,
+      },
+    });
+    const rocks = await response.json();
+    setRocksState(rocks);
+  };
 
-    return <BrowserRouter>
-        <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route element={<Authorized />}>
-                <Route path="/" element={<Home />} />
-                <Route path="/allrocks" element={<RockList rocks={rocksState} fetchRocks={fetchRocksFromAPI} />} />
-                <Route path="/create" element={<RockForm fetchRocks={fetchRocksFromAPI} />} />
-                <Route path="/mine" element={<RockList rocks={rocksState} fetchRocks={fetchRocksFromAPI} />} />
-            </Route>
-        </Routes>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route element={<Authorized />}>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/allrocks"
+            element={
+              <RockList
+                rocks={rocksState}
+                fetchRocks={fetchRocksFromAPI}
+                showAll={true}
+              />
+            }
+          />
+          <Route
+            path="/create"
+            element={<RockForm fetchRocks={fetchRocksFromAPI} />}
+          />
+          <Route
+            path="/mine"
+            element={
+              <RockList
+                rocks={rocksState}
+                fetchRocks={fetchRocksFromAPI}
+                showAll={false}
+              />
+            }
+          />
+        </Route>
+      </Routes>
     </BrowserRouter>
-}
+  );
+};
--- src/components/RockList.jsx
+++ src/components/RockList.jsx
@@ -1,23 +1,59 @@
-import { useEffect } from "react"
+import { useEffect } from 'react';
 
-export const RockList = ({ rocks, fetchRocks }) => {
-    useEffect(() => {
-    }, [])
+export const RockList = ({ rocks, fetchRocks, showAll }) => {
+  useEffect(() => {
+    fetchRocks(showAll);
+  }, [showAll]);
 
-    const displayRocks = () => {
-        if (rocks && rocks.length) {
-            return rocks.map(rock => <div key={`key-${rock.id}`} className="border p-5 border-solid hover:bg-fuchsia-500 hover:text-violet-50 rounded-md border-violet-900 mt-5 bg-slate-50">
-                {rock.name} ({rock.type.label})
-            </div>)
-        }
-
-        return <h3>Loading Rocks...</h3>
+  const displayRocks = () => {
+    if (rocks && rocks.length) {
+      return rocks.map((rock) => (
+        <div
+          key={`key-${rock.id}`}
+          className="border p-5 border-solid hover:bg-fuchsia-500 hover:text-violet-50 rounded-md border-violet-900 mt-5 bg-slate-50"
+        >
+          {rock.name} ({rock.type.label})
+          <div>
+            In the collection of {rock.user?.first_name} {rock.user?.last_name}{' '}
+          </div>
+          {showAll ? (
+            ''
+          ) : (
+            <div>
+              <button
+                onClick={async () => {
+                  const response = await fetch(
+                    `http://localhost:8000/rocks/${rock.id}`,
+                    {
+                      method: 'DELETE',
+                      headers: {
+                        Authorization: `Token ${
+                          JSON.parse(localStorage.getItem('rock_token')).token
+                        }`,
+                      },
+                    }
+                  );
+                  if (response.status === 204) {
+                    fetchRocks(showAll);
+                  }
+                }}
+                className="border border-solid bg-red-700 text-white p-1"
+              >
+                Delete
+              </button>
+            </div>
+          )}
+        </div>
+      ));
     }
 
-    return (
-        <>
-            <h1 className="text-3xl">Rock List</h1>
-            {displayRocks()}
-        </>
-    )
-}
+    return <h3>Loading Rocks...</h3>;
+  };
+
+  return (
+    <>
+      <h1 className="text-3xl">Rock List</h1>
+      {displayRocks()}
+    </>
+  );
+};
